0970d52e6493fb9685c28240cb5a0566dfe28be7
daveBlue854 <dudiblau11@gmail.com>
2020-06-25 15:17:52 +0300

    some tests

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -10,3 +10,22 @@ class TestOutcome(TestCase):
     def testRepr(self):
         o = roulette.Outcome('red', 1)
         self.assertEqual(repr(o), "Outcome : name='red', odds=1")
+
+    def testEquality(self):
+        o1 = roulette.Outcome('red', 1)
+        o2 = roulette.Outcome('red', 1)
+        self.assertEqual(o1, o2)
+
+    def testInEquality(self):
+        o1 = roulette.Outcome('blue', 1)
+        o2 = roulette.Outcome('red', 1)
+        self.assertNotEqual(o1, o2)
+
+    def testHash(self):
+        o1 = roulette.Outcome('red', 1)
+        o2 = roulette.Outcome('red', 1)
+        self.assertEqual(hash(o1), hash(o2))
+
+    def testWinAmount(self):
+        o1 = roulette.Outcome('red', 2)
+        self.assertEqual(o1.winAmount(3), 6)
