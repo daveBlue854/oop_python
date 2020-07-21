e534ac9fc98bb0046813b9cca2dc343db6126be9
daveBlue854 <dudiblau11@gmail.com>
2020-07-21 16:47:04 +0300

    added part of binBuilder and some tests

--- src/roulette.py
+++ src/roulette.py
@@ -1,6 +1,11 @@
-import os
 from dataclasses import dataclass
 import random
+from enum import Enum
+
+
+class BetOdds(Enum):
+    STRAIGHT_BET = 35
+    SPLIT_BET = 17
 
 
 @dataclass(frozen=True)
@@ -37,3 +42,35 @@ class Wheel():
     def next(self):
         randIndex = self.rng.randint(0, 37)
         return self.bins[randIndex]
+
+
+class BinBuilder():
+    def __init__(self, wheel: Wheel):
+        self.wheel = wheel
+
+    def generateStraightBets(self):
+        for i in range(37):
+            outcome = Outcome(f"{i}", BetOdds.STRAIGHT_BET)
+            self.wheel.addOutcome(i, outcome)
+
+        straightBetZeroZeroOutcome = Outcome("00", BetOdds.STRAIGHT_BET)
+        self.wheel.addOutcome(37, straightBetZeroZeroOutcome)
+
+    def generateSplitBets(self):
+        middleCol = [i for i in range(2, 36, 3)]
+        for n in middleCol:
+            leftSplit = Outcome(f"split {n - 1},{n}", BetOdds.SPLIT_BET)
+            self.wheel.addOutcome(n - 1, leftSplit)
+            self.wheel.addOutcome(n, leftSplit)
+
+            rightSplit = Outcome(f"split {n},{n + 1}", BetOdds.SPLIT_BET)
+            self.wheel.addOutcome(n, rightSplit)
+            self.wheel.addOutcome(n + 1, rightSplit)
+
+        for n in range(4, 37):
+            upDownSplit = Outcome(f"split {n - 3},{n}", BetOdds.SPLIT_BET)
+            self.wheel.addOutcome(n - 3, upDownSplit)
+            self.wheel.addOutcome(n, upDownSplit)
+
+    def generateStreetBets(self):
+        pass
