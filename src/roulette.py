69ef3a2bddea37428e11eba235b0b8dfe29a5f28
daveBlue854 <dudiblau11@gmail.com>
2020-07-26 13:09:30 +0300

    almost finished binBuilder

--- src/roulette.py
+++ src/roulette.py
@@ -8,7 +8,11 @@ class BetOdds(Enum):
     SPLIT = 17
     STREET = 11
     CORNER = 8
+    FIVE_BET = 6
     LINE = 5
+    DOZEN = 2
+    COLUMN = 2
+    EVEN_MONEY = 1
 
 
 @dataclass(frozen=True)
@@ -50,6 +54,9 @@ class Wheel():
 class BinBuilder():
     def __init__(self, wheel: Wheel):
         self.wheel = wheel
+
+    def buildBins(self):
+        pass
 
     def straightBets(self):
         for i in range(37):
@@ -100,3 +107,54 @@ class BinBuilder():
             line = Outcome(f"line {str(lineNums)}", BetOdds.LINE)
             for num in lineNums:
                 self.wheel.addOutcome(num, line)
+
+    def dozenBets(self):
+        for i in range(3):
+            dozenNums = tuple(k + 1 for k in range(i * 12, i * 12 + 12))
+            dozen = Outcome(f"dozen {str(dozenNums)}", BetOdds.DOZEN)
+            for num in dozenNums:
+                self.wheel.addOutcome(num, dozen)
+
+    def columnBets(self):
+        for i in range(3):
+            columnNums = tuple(j for j in range(i + 1, 35 + i, 3))
+            column = Outcome(f"column {str(columnNums)}", BetOdds.COLUMN)
+            for num in columnNums:
+                self.wheel.addOutcome(num, column)
+
+    def evenMoneyBets(self):
+        redNums = (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36)
+        red = Outcome(f"red", BetOdds.EVEN_MONEY)
+        for num in redNums:
+            self.wheel.addOutcome(num, red)
+
+        blackNums = tuple(i for i in range(1, 36) if i not in redNums)
+        black = Outcome(f"black", BetOdds.EVEN_MONEY)
+        for num in blackNums:
+            self.wheel.addOutcome(num, black)
+
+        oddNums = tuple(i for i in range(1, 36) if i % 2 == 1)
+        odd = Outcome(f"odd", BetOdds.EVEN_MONEY)
+        for num in oddNums:
+            self.wheel.addOutcome(num, odd)
+
+        evenNums = tuple(i for i in range(1, 36) if i % 2 == 0)
+        even = Outcome(f"even", BetOdds.EVEN_MONEY)
+        for num in evenNums:
+            self.wheel.addOutcome(num, even)
+
+        lowNums = tuple(i for i in range(1, 36) if i < 19)
+        low = Outcome(f"low", BetOdds.EVEN_MONEY)
+        for num in lowNums:
+            self.wheel.addOutcome(num, low)
+
+        highNums = tuple(i for i in range(1, 36) if i > 19)
+        high = Outcome(f"high", BetOdds.EVEN_MONEY)
+        for num in highNums:
+            self.wheel.addOutcome(num, high)
+
+    def fiveBet(self):
+        fiveBetNums = [0, 37, 1, 2, 3]
+        fiveBet = Outcome('fiveBet', BetOdds.FIVE_BET)
+        for num in fiveBetNums:
+            self.wheel.addOutcome(num, fiveBet)
--- tests/test_binBuilder.py
+++ tests/test_binBuilder.py
@@ -3,7 +3,7 @@ from src.roulette import Wheel, BinBuilder, Bin, Outcome, BetOdds
 
 
 class testBinBuilder(TestCase):
-    def init(self):
+    def __init(self):
         w = Wheel()
         a = BinBuilder(w)
         return (a, w)
@@ -14,61 +14,115 @@ class testBinBuilder(TestCase):
         self.assertEqual(type(binBuilder), type(BinBuilder(w)))
         self.assertEqual(type(binBuilder.wheel.get(0)), type(Bin()))
 
-    def testStraightBets(self):
-        (binBuilder, wheel) = self.init()
+    def testBinBuild(self):
+        self.assertTrue(True)
+
+    def testStraightBets(self, ):
+        (binBuilder, wheel) = self.__init()
         binBuilder.straightBets()
         zeroBin = wheel.get(0)
         zeroZeroBin = wheel.get(37)
         oneBin = wheel.get(1)
-        self.assertTrue(Outcome("0", BetOdds.STRAIGHT) in zeroBin)
-        self.assertTrue(Outcome("00", BetOdds.STRAIGHT) in zeroZeroBin)
-        self.assertTrue(Outcome("1", BetOdds.STRAIGHT) in oneBin)
+        self.assertIn(Outcome("0", BetOdds.STRAIGHT), zeroBin)
+        self.assertIn(Outcome("00", BetOdds.STRAIGHT), zeroZeroBin)
+        self.assertIn(Outcome("1", BetOdds.STRAIGHT), oneBin)
 
     def testSplitBets(self):
-        (binBuilder, wheel) = self.init()
+        (binBuilder, wheel) = self.__init()
         binBuilder.splitBets()
         oneBin = wheel.get(1)
         thirtySixBin = wheel.get(36)
-        self.assertTrue(Outcome("split 1,2", BetOdds.SPLIT) in oneBin)
-        self.assertTrue(Outcome("split 1,4", BetOdds.SPLIT) in oneBin)
+        self.assertIn(Outcome("split 1,2", BetOdds.SPLIT), oneBin)
+        self.assertIn(Outcome("split 1,4", BetOdds.SPLIT), oneBin)
 
-        self.assertTrue(Outcome("split 35,36", BetOdds.SPLIT) in thirtySixBin)
-        self.assertTrue(Outcome("split 33,36", BetOdds.SPLIT) in thirtySixBin)
+        self.assertIn(Outcome("split 35,36", BetOdds.SPLIT), thirtySixBin)
+        self.assertIn(Outcome("split 33,36", BetOdds.SPLIT), thirtySixBin)
 
     def testStreetBets(self):
-        (binBuilder, wheel) = self.init()
+        (binBuilder, wheel) = self.__init()
         binBuilder.streetBets()
         oneBin = wheel.get(1)
         thirtySixBin = wheel.get(36)
-        self.assertTrue(Outcome("street (1, 2, 3)", BetOdds.STREET) in oneBin)
-        self.assertTrue(Outcome("street (34, 35, 36)", BetOdds.STREET) in thirtySixBin)
+        self.assertIn(Outcome("street (1, 2, 3)", BetOdds.STREET), oneBin)
+        self.assertIn(Outcome("street (34, 35, 36)", BetOdds.STREET), thirtySixBin)
 
     def testCornerBets(self):
-        (binBuilder, wheel) = self.init()
+        (binBuilder, wheel) = self.__init()
         binBuilder.cornerBets()
         oneBin = wheel.get(1)
         twoBin = wheel.get(2)
         fiveBin = wheel.get(5)
 
-        self.assertTrue(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER) in oneBin)
+        self.assertIn(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER), oneBin)
 
-        self.assertTrue(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER) in twoBin)
-        self.assertTrue(Outcome("corner (2, 3, 5, 6)", BetOdds.CORNER) in twoBin)
+        self.assertIn(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER), twoBin)
+        self.assertIn(Outcome("corner (2, 3, 5, 6)", BetOdds.CORNER), twoBin)
 
-        self.assertTrue(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER) in fiveBin)
-        self.assertTrue(Outcome("corner (2, 3, 5, 6)", BetOdds.CORNER) in fiveBin)
-        self.assertTrue(Outcome("corner (4, 5, 7, 8)", BetOdds.CORNER) in fiveBin)
-        self.assertTrue(Outcome("corner (5, 6, 8, 9)", BetOdds.CORNER) in fiveBin)
+        self.assertIn(Outcome("corner (1, 2, 4, 5)", BetOdds.CORNER), fiveBin)
+        self.assertIn(Outcome("corner (2, 3, 5, 6)", BetOdds.CORNER), fiveBin)
+        self.assertIn(Outcome("corner (4, 5, 7, 8)", BetOdds.CORNER), fiveBin)
+        self.assertIn(Outcome("corner (5, 6, 8, 9)", BetOdds.CORNER), fiveBin)
 
     def testLineBets(self):
-        (binBuilder, wheel) = self.init()
+        (binBuilder, wheel) = self.__init()
         binBuilder.lineBets()
         oneBin = wheel.get(1)
         fourBin = wheel.get(4)
 
-        self.assertTrue(Outcome("line (1, 2, 3, 4, 5, 6)", BetOdds.LINE) in oneBin)
+        self.assertIn(Outcome("line (1, 2, 3, 4, 5, 6)", BetOdds.LINE), oneBin)
         self.assertEqual(len(oneBin), 1)
 
-        self.assertTrue(Outcome("line (1, 2, 3, 4, 5, 6)", BetOdds.LINE) in fourBin)
-        self.assertTrue(Outcome("line (4, 5, 6, 7, 8, 9)", BetOdds.LINE) in fourBin)
+        self.assertIn(Outcome("line (1, 2, 3, 4, 5, 6)", BetOdds.LINE), fourBin)
+        self.assertIn(Outcome("line (4, 5, 6, 7, 8, 9)", BetOdds.LINE), fourBin)
         self.assertEqual(len(fourBin), 2)
+
+    def testDozenBets(self):
+        (binBuilder, wheel) = self.__init()
+        binBuilder.dozenBets()
+        oneBin = wheel.get(1)
+        thirteenBin = wheel.get(13)
+        thirtySixBin = wheel.get(36)
+
+        self.assertIn(Outcome('dozen (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)', BetOdds.DOZEN), oneBin)
+        self.assertIn(Outcome('dozen (13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)', BetOdds.DOZEN), thirteenBin)
+        self.assertIn(
+            Outcome('dozen (25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36)', BetOdds.DOZEN), thirtySixBin)
+
+    def testColumnBets(self):
+        (binBuilder, wheel) = self.__init()
+        binBuilder.columnBets()
+        oneBin = wheel.get(1)
+        threeBin = wheel.get(3)
+        thirtyFiveBin = wheel.get(35)
+
+        self.assertIn(Outcome('column (1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34)', BetOdds.COLUMN), oneBin)
+        self.assertIn(
+            Outcome('column (2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35)', BetOdds.COLUMN), thirtyFiveBin)
+        self.assertIn(Outcome('column (3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36)', BetOdds.COLUMN), threeBin)
+
+    def testEvenMoney(self):
+        (binBuilder, wheel) = self.__init()
+        binBuilder.evenMoneyBets()
+        oneBin = wheel.get(1)
+        twentyEightBin = wheel.get(28)
+
+        oneBinEvenMoneyOutcomes = [Outcome('red', BetOdds.EVEN_MONEY),
+                                   Outcome('odd', BetOdds.EVEN_MONEY),
+                                   Outcome('low', BetOdds.EVEN_MONEY)]
+        for outcome in oneBinEvenMoneyOutcomes:
+            self.assertIn(outcome, oneBin)
+
+        twentyEightBinEvenMoneyOutcomes = [Outcome('black', BetOdds.EVEN_MONEY),
+                                           Outcome('even', BetOdds.EVEN_MONEY),
+                                           Outcome('high', BetOdds.EVEN_MONEY)]
+
+        for outcome in twentyEightBinEvenMoneyOutcomes:
+            self.assertIn(outcome, twentyEightBin)
+
+    def testFiveBet(self):
+        (binBuilder, wheel) = self.__init()
+        binBuilder.fiveBet()
+        fiveBetBins = [wheel.get(0), wheel.get(37), wheel.get(1), wheel.get(2), wheel.get(3)]
+        fiveBetOutcome = Outcome('fiveBet', BetOdds.FIVE_BET)
+        for fBin in fiveBetBins:
+            self.assertIn(fiveBetOutcome, fBin)
