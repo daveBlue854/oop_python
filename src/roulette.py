e6e56ffc96191ac2303add0b79e6a0093db83ccf
daveBlue854 <dudiblau11@gmail.com>
2020-09-16 14:23:31 +0300

    added Passenger57 class and tests

--- src/roulette.py
+++ src/roulette.py
@@ -228,3 +228,21 @@ class Table:
         s = 'Bets list: '
         betsList = [str(bet) for bet in self.bets]
         return s + str(betsList)
+
+
+class Passenger57:
+    BLACK_OUTCOME_NAME = 'black'
+
+    def __init__(self, table: Table, wheel: Wheel) -> None:
+        self.table = table
+        self.black = wheel.getOutcomeByName(Passenger57.BLACK_OUTCOME_NAME)
+
+    def placeBets(self) -> None:
+        blackBet = Bet(10, self.black)
+        self.table.placeBet(blackBet)
+
+    def win(self, bet: Bet) -> None:
+        moneyWon = bet.winAmont()
+
+    def lose(self, bet: Bet) -> None:
+        moneyLost = bet.loseAmount()
---
+++ tests/test_Passenger57.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from src.roulette import *
+
+
+class TestPassenger57(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.w = Wheel()
+        binBuilder = BinBuilder(cls.w)
+        binBuilder.buildBins()
+        cls.t = Table()
+        cls.p57 = Passenger57(cls.t, cls.w)
+
+    def testInit(self):
+        self.assertEqual(self.p57.table, self.t)
+        self.assertEqual(self.p57.black, self.w.getOutcomeByName('black'))
+
+    def testPlaceBets(self):
+        self.p57.placeBets()
+        self.assertEqual(len(self.t.bets), 1)
+        placedBet: Bet = self.t.bets[0]
+        self.assertEqual(placedBet.__str__(), Bet(10, self.w.getOutcomeByName('black')).__str__())
